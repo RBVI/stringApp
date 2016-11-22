c67acdabcd3711fa8c9c65d51b8cd9d75fed6e5d
Scooter Morris <scooter@cgl.ucsf.edu>
2016-11-22 07:04:42 -0800

    POST now seems to be working for STITCH

--- src/main/java/edu/ucsf/rbvi/stringApp/internal/model/StringNetwork.java
+++ src/main/java/edu/ucsf/rbvi/stringApp/internal/model/StringNetwork.java
@@ -56,11 +56,16 @@ public class StringNetwork {
 		System.out.println("URL: "+url+"?species="+Integer.toString(taxon)+"&caller_identity="+StringManager.CallerIdentity+"&identifiers="+encTerms);
 		Object results;
 		// Get the results
+		//
+		/* At one point STITCH didn't work with POST
 		if (!useSTITCH) {
 			results = HttpUtils.postJSON(url, args, manager);
 		} else {
-			results = HttpUtils.getJSON(url, args, manager);
+			results = HttpUtils.postJSON(url, args, manager);
 		}
+		*/
+		results = HttpUtils.postJSON(url, args, manager);
+
 		if (results == null) return null;
 		// System.out.println("Got results");
 		annotations = Annotation.getAnnotations(results, terms);
