dcce370ff3aef24beda4f8290a4053f7eaf5f13c
Scooter Morris <scooter@cgl.ucsf.edu>
2017-11-05 14:55:11 -0800

    Fix merge error

--- src/main/java/edu/ucsf/rbvi/stringApp/internal/model/Species.java
+++ src/main/java/edu/ucsf/rbvi/stringApp/internal/model/Species.java
@@ -137,13 +137,6 @@ public class Species implements Comparable<Species> {
 			}
 		}
 		return partners;
-
-	public static Species getSpecies(String speciesName) {
-		for (Species s: allSpecies) {
-			if (s.getName().equalsIgnoreCase(speciesName))
-				return s;
-		}
-		return null;
 	}
 
 	public static String getSpeciesName(String taxId) {
