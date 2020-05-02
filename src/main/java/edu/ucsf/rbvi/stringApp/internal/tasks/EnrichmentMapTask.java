cfb9495e4366ae29f487be40e7cac33ab55029de
Scooter Morris <scooter@cgl.ucsf.edu>
2020-05-02 16:30:08 -0700

    Make sure we've got the right OSGi versions, etc. and also fix a bug in the network naming

--- src/main/java/edu/ucsf/rbvi/stringApp/internal/tasks/EnrichmentMapTask.java
+++ src/main/java/edu/ucsf/rbvi/stringApp/internal/tasks/EnrichmentMapTask.java
@@ -23,18 +23,25 @@ public class EnrichmentMapTask extends AbstractTask {
 	public String mapName = "STRING Enrichment Map";
 
 	// Similarity cutoff
-	@Tunable(description="Similarity cutoff", params="slider=true")
+	@Tunable(description="Similarity cutoff", 
+	         longDescription="The cutoff for the Jaccard similarity of terms.  "+
+					                 "Higher values mean fewer terms but more understandable networks. "+
+													 "Good values for disease networks are 0.8 or 0.9", params="slider=true")
 	public BoundedDouble similarity = new BoundedDouble(0.0, 0.5, 1.0, true, true);
 
 	// FDR cutoff
-	@Tunable(description="FDR cutoff", params="slider=true")
+	@Tunable(description="FDR cutoff", 
+	         longDescription="The FDR cutoff for terms.  Terms with FDR values larger than this will be dropped",
+					 params="slider=true")
 	public BoundedDouble FDRcutoff = new BoundedDouble(0.0, 0.05, 0.5, true, false);
 	
 	public EnrichmentMapTask(final StringManager manager, final CyNetwork network, final CyTable filteredEnrichmentTable) {
 		this.manager = manager;
 		this.filteredEnrichmentTable = filteredEnrichmentTable;
 		String netName = network.getRow(network).get(CyNetwork.NAME, String.class);
-		if (netName.startsWith("String Network")) {
+		if (netName.equals("String Network")) {
+			mapName = "Enrichment Map - String";
+		} else if (netName.startsWith("String Network")) {
 			mapName = "Enrichment Map "+netName.substring(15);
 		} else {
 			mapName = "Enrichment Map - "+netName;
