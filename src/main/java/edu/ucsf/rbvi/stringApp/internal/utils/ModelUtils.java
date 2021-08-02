285c8eeb736db6f46322d1c8c7b70951158ddfc1
doncheva <ndoncheva@gmail.com>
2021-08-02 16:43:09 +0200

    Added preliminary species file for 11.5 to resources, but they are not used yet.

--- src/main/java/edu/ucsf/rbvi/stringApp/internal/utils/ModelUtils.java
+++ src/main/java/edu/ucsf/rbvi/stringApp/internal/utils/ModelUtils.java
@@ -660,7 +660,7 @@ public class ModelUtils {
 			if (nSpecies != null && !nSpecies.equals("") && !species.contains(nSpecies)) {
 				Species theSpecies = Species.getSpecies(nSpecies);
 				// TODO: This is kind of a hack for now and will be updated once we get the kingdom data from the server 
-				if (theSpecies != null && (theSpecies.getType().equals("core") || theSpecies.getType().equals("periphery")))
+				if (theSpecies != null && (theSpecies.getType().equals("core") || theSpecies.getType().equals("periphery") || theSpecies.getType().equals("mapped")))
 					species.add(nSpecies);
 			}
 		}
